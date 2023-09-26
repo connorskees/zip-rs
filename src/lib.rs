0e090ecbddaabd3eb01166384d419012746dd0e8
Connor Skees <connor1skees@gmail.com>
2023-09-26 05:39:11 +0000

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@
  * Zip file parsing library
  *
  * ```no_run
- * let file = ZipArchive::from_path("./foo.zip").unwrap();
+ * # use zip_rs::ZipArchive;
+ * let mut archive = ZipArchive::from_path("./foo.zip").unwrap();
  *
- * for file in files {
+ * for file in archive.files() {
  *   // ...
  * }
  * ```
@@ -36,9 +37,9 @@ mod common;
 mod error;
 mod parse;
 
-const KB: u64 = 1024;
-const MB: u64 = 1024 * KB;
-const GB: u64 = 1024 * MB;
+const KB: usize = 1024;
+const MB: usize = 1024 * KB;
+const GB: usize = 1024 * MB;
 
 /// An entire ZIP archive file
 #[derive(Debug)]
@@ -48,6 +49,7 @@ pub struct ZipArchive<'a, B: Deref<Target = [u8]>> {
 }
 
 impl<'a> ZipArchive<'a, memmap::Mmap> {
+    /// Parse a [`ZipArchive`] from a file path. The contents are memory mapped.
     pub fn from_path(path: impl AsRef<Path>) -> Result<Self, ZipParseError> {
         let file = File::open(path)?;
         let buffer = unsafe { memmap::Mmap::map(&file) }?;
@@ -57,6 +59,7 @@ impl<'a> ZipArchive<'a, memmap::Mmap> {
 }
 
 impl<'a, B: Deref<Target = [u8]>> ZipArchive<'a, B> {
+    /// Parse a [`ZipArchive`] from an existing buffer in memory
     pub fn from_buffer(buffer: B) -> Result<Self, ZipParseError> {
         let mut parser = Parser::new(buffer);
 
@@ -68,9 +71,11 @@ impl<'a, B: Deref<Target = [u8]>> ZipArchive<'a, B> {
         })
     }
 
+    /// Lazily iterate over the files in this archive
     pub fn files<'b>(
         &'b mut self,
     ) -> impl Iterator<Item = Result<CompressedZipFile<'a>, ZipParseError>> + 'b {
+        // todo: can we avoid this clone by wrapping self.parser in RefCell?
         let files = self.central_directory.files.clone();
 
         files
@@ -114,7 +119,7 @@ pub struct Metadata<'a> {
     pub crc: u32,
 }
 
-/// A single compressed zip file
+/// A single compressed ZIP file
 #[derive(Debug)]
 pub struct CompressedZipFile<'a> {
     pub metadata: Metadata<'a>,
@@ -128,9 +133,16 @@ impl<'a> CompressedZipFile<'a> {
 
     /// Efficiently writes decompressed contents to sink without loading full
     /// decompressed contents into memory
-    pub fn write(&self, w: &mut dyn Write) -> Result<(), ZipParseError> {
-        // disallow decompressing files over 5gb to avoid zip bombs
-        if self.metadata.uncompressed_size >= 5 * GB {
+    ///
+    /// `limit` controls the max uncompressed file size that will be accepted. A
+    /// `limit` of `None` implies no limit. Note that setting too high of a limit
+    /// can make decoders susceptible to DoS through ZIP bombs or other means.
+    pub fn write_with_limit(
+        &self,
+        w: &mut dyn Write,
+        limit: Option<usize>,
+    ) -> Result<(), ZipParseError> {
+        if Some(self.metadata.uncompressed_size as usize) >= limit {
             return Err(ZipParseError::FileTooLarge(self.metadata.uncompressed_size));
         }
 
@@ -153,10 +165,25 @@ impl<'a> CompressedZipFile<'a> {
         Ok(())
     }
 
-    /// Decompress contents in one go into memory
-    pub fn decompressed_contents(&self) -> Result<Cow<[u8]>, ZipParseError> {
-        // disallow decompressing files over 5gb to avoid zip bombs
-        if self.metadata.uncompressed_size >= 5 * GB {
+    /// Efficiently writes decompressed contents to sink without loading full
+    /// decompressed contents into memory.
+    ///
+    /// This method uses the default limit of 8 gigabytes. See
+    /// [CompressedZipFile::write_with_limit] to configure this limit.
+    pub fn write(&self, w: &mut dyn Write) -> Result<(), ZipParseError> {
+        self.write_with_limit(w, Some(8 * GB))
+    }
+
+    /// Decompress full contents into memory
+    ///
+    /// `limit` controls the max uncompressed file size that will be accepted. A
+    /// `limit` of `None` implies no limit. Note that setting too high of a limit
+    /// can make decoders susceptible to DoS through ZIP bombs or other means.
+    pub fn decompressed_contents_with_limit(
+        &self,
+        limit: Option<usize>,
+    ) -> Result<Cow<[u8]>, ZipParseError> {
+        if Some(self.metadata.uncompressed_size as usize) >= limit {
             return Err(ZipParseError::FileTooLarge(self.metadata.uncompressed_size));
         }
 
@@ -173,14 +200,39 @@ impl<'a> CompressedZipFile<'a> {
         }
     }
 
+    /// Decompress full contents into memory
+    ///
+    /// This method uses the default limit of 8 gigabytes. See
+    /// [CompressedZipFile::decompressed_contents_with_limit] to configure this
+    /// limit.
+    pub fn decompressed_contents(&self) -> Result<Cow<[u8]>, ZipParseError> {
+        self.decompressed_contents_with_limit(Some(8 * GB))
+    }
+
+    /// This file's `Path` inside the ZIP archive.
+    ///
+    /// Note that this path may reference file paths outside the archive through
+    /// the use of absolute paths or the parent directory (`..`). The full file path
+    /// should not be used when interacting with the host file system if the ZIP
+    /// file is untrusted.
     pub fn file_path(&self) -> &Path {
         &Path::new(OsStr::from_bytes(self.metadata.name))
     }
 
+    /// The raw bytes of this file's path inside the ZIP archive.
+    ///
+    /// Note that this path may reference file paths outside the archive through
+    /// the use of absolute paths or the parent directory (`..`). The full file path
+    /// should not be used when interacting with the host file system if the ZIP
+    /// file is untrusted.
     pub fn file_path_bytes(&self) -> &'a [u8] {
         self.metadata.name
     }
 
+    /// The algorithm used to compress this file.
+    ///
+    /// This is typically [`CompressionMethodName::None`] or
+    /// [`CompressionMethodName::Deflate`].
     pub fn compression_method(&self) -> CompressionMethod {
         self.metadata.compression_method
     }
@@ -194,7 +246,7 @@ pub struct CentralDirectory<'a> {
 
 #[cfg(test)]
 mod test {
-    use crate::ZipArchive;
+    use crate::{ZipArchive, MB};
 
     #[test]
     #[should_panic]
@@ -204,7 +256,8 @@ mod test {
         for file in bomb.files() {
             let file = file.unwrap();
 
-            file.decompressed_contents().unwrap();
+            file.decompressed_contents_with_limit(Some(20 * MB))
+                .unwrap();
         }
     }
 }
